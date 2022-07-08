35a20bddd996052fa54dfc8e8e44b49d9a655888
Duong Minh <tnmnh1105@gmail.com>
2022-07-08 22:56:40 +0700

    feat:move store list to home and change store tab to gift

--- src/pages/home/child/variant-01/index.js
+++ src/pages/home/child/variant-01/index.js
@@ -1,8 +1,21 @@
+
+import { changeDefaultStore } from "../../../../store/actions/store";
+import { navigateTo } from "../../../../helper";
+
 Component({
   props: {
     headerType: '',
     banners: [],
     status: '',
     campaigns: [],
+    stores: [],
   },
-});
+  methods:{
+    onStoreSelect(e) {
+      const id = e.target.dataset.id;
+      changeDefaultStore(id);
+      navigateTo("store-detail", { id });
+    },
+  }
+}
+);
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,23 @@
 import { getData } from '../../services';
+import { $page } from "@tiki.vn/redux-miniprogram-bindings";
 
+import { getAllStore, changeDefaultStore } from "../../store/actions/store";
+import { navigateTo } from "../../helper";
+// Page
 Page({
   data: {
     status: 'LOADING',
     headerType: 'DEFAULT',
     campaigns: [1, 2, 3, 4],
     banners: [1, 2, 3],
+    stores: [],
   },
   async loadData() {
     try {
-      const [banners, campaigns] = await Promise.all([
+      const [banners, campaigns, stores] = await Promise.all([
         getData('banners'),
         getData('campaigns'),
+         getData('stores')
       ]);
 
       this.setData({
@@ -19,6 +25,7 @@ Page({
         banners,
         campaigns,
         status: 'SUCCESS',
+        stores,
       });
     } catch (err) {
       this.setData({
