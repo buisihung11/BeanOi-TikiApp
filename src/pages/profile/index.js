ddeb0adaa3d90d699f905fa0edb8899478cb51a2
HungHcc <buisihung321@gmail.com>
2022-07-09 14:04:51 +0700

    feat: get user auth code from tiki

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,9 +1,21 @@
-Page({
-  data: {
-    status: 'LOADING',
-    headerType: 'DEFAULT',
-  },
+import myx from "../../helper/myx";
+import { $page } from "@tiki.vn/redux-miniprogram-bindings";
+import { setUserInfo } from "../../store/actions/auth";
 
+$page({
+  mapState: [(state) => ({ user: state.auth.user })],
+  mapDispatch: { setUserInfo },
+})({
+  data: {
+    status: "LOADING",
+    headerType: "DEFAULT",
+  },
+  async onLogin() {
+    const res = await myx.getAuthCode();
+    const userInfo = await myx.getUserInfo();
+    console.log("auth", res.authCode, userInfo);
+    await this.setUserInfo({ ...userInfo, authCode: res.authCode });
+  },
   loadData() {},
 
   onLoad(query) {},
@@ -12,12 +24,12 @@ Page({
     if (e.scrollTop >= 50)
       this.setData({
         ...this.data,
-        headerType: 'SCROLLED',
+        headerType: "SCROLLED",
       });
   },
   onPhoneCall() {
     my.makePhoneCall({
-      number: '0342909090',
+      number: "0342909090",
       success: (res) => {},
       fail: (e) => {},
     });
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -12,6 +12,11 @@ export default function auth(state = initialState, action) {
         ...state,
         token: action.data,
       };
+    case c.SET_USER_INFO:
+      return {
+        ...state,
+        user: action.payload,
+      };
     case c.REFRESH_USER_TOKEN:
       return {
         ...state,
